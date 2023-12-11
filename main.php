b8baecdf68f15cb97b756ad7f29431603faf8518
Wildanamru <90967544+Wildanamru@users.noreply.github.com>
2023-12-11 10:12:57 +0700

    Update main.php

--- main.php
+++ main.php
@@ -15,3 +15,4 @@ $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $productRoute = new ProductRoutes();
 $productRoute->handle($method, $path);
 
+//RUN MAIN PHP DENGAN "PHP -S localhost:8000 main.hph"
